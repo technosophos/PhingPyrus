f568ae6ca0f2a8cc7c35144cd38be73d9dfa3ddf
Matt Butcher <technosophos@gmail.com>
2012-03-02 13:21:41 -0600

    Updated with Pyrus build info.
    Pyrus can now be used to package this project. Always a useful
    thing.

---
+++ pear2/PhingPyrus/src/PhingPyrus/Main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PhingPyrus\Main
+ *
+ * PHP version 5
+ *
+ * @category  Yourcategory
+ * @package   PhingPyrus
+ * @author    Your Name <handle@php.net>
+ * @copyright 2012 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://svn.pear.querypath.org/???
+ */
+
+/**
+ * Main class for PhingPyrus
+ *
+ * @category  Yourcategory
+ * @package   PhingPyrus
+ * @author    Your Name <handle@php.net>
+ * @copyright 2012 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.pear.querypath.org/???
+ */
+namespace PhingPyrus;
+class Main
+{
+}
